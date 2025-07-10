9bfffb56de453b89581eba0cbd783b13c9da7ab9
Ikujebi <133238121+Ikujebi@users.noreply.github.com>
2025-07-10 16:37:55 +0100

    Update page.tsx

--- src/app/text/page.tsx
+++ src/app/text/page.tsx
@@ -1,26 +1,24 @@
 "use client";
+
 import Image from "next/image";
-import { IoTriangle } from "react-icons/io5";
+
 import warehouse from "../assets/img/warehouse.png";
 
-
+const Card2 = () => {
   return (
     <div className="bg-[#040404] p-2 rounded-xl shadow-md text-center w-[14rem]">
-  
-      <div className="flex">
-        <div className="bg-white rounded-[70%] flex justify-center items-center h-3 w-3">
-          <Image height={100} width={200} src={warehouse.src} alt="News icon" className="w-2" />
+      <div className="flex items-center">
+        <div className="bg-white rounded-full flex justify-center items-center h-3 w-3">
+          <Image
+            height={100}
+            width={200}
+            src={warehouse.src}
+            alt="Depot icon"
+            className="w-2 h-2 object-contain"
+          />
         </div>
         <p className="text-white text-[.45rem] mt-[.1rem] pl-1 mb-[.5rem]">Depot</p>
       </div>
-
-      
-      
-
-     
-
- 
-     
     </div>
   );
 };
